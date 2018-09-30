e199ea5bac7c3bdf29b8fe518466034e871c59dc
charlie <wangchuanyin2000@163.com>
2018-10-01 07:53:16 +0800

    example inital
    this is a vue project with coreUI

---
+++ src/views/questionPager/askLeft.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="col-md-3 ask-left"></div>
+</template>
+<script>
+export default {
+    name:'askLeft'
+}
+</script>
+<style>
+
+.ask-left{
+    
+    height: 200px;
+    border: 1px solid;
+}
+
+
+
+</style>
---
+++ src/views/questionPager/askPager.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="row">
+   <ask-left/>
+   <ask-main/>
+   <ask-setting/>
+</div>
+</template>
+<script>
+import askLeft from "./askLeft"
+import askMain from "./askMain"
+import askSetting from "./askSetting"
+
+export default {
+    name:'askPager',
+    components:{askLeft,askMain,askSetting}
+}
+</script>
+<style>
+
+</style>
